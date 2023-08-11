1d71e3b5c39dc03a111dd20e81f5d31f5929345c
BittVivek08 <vivekkimar706277@gmail.com>
2023-08-11 11:41:57 +0530

    Rainier_ecommerse created

---
+++ src/main/java/com/rainier/RainierEcommerseApplication.java
@@ -0,0 +1,13 @@
+package com.rainier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainierEcommerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainierEcommerseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rainier/RainierEcommerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rainier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainierEcommerseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
