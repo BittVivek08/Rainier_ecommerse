b4e2c22bdeea092798884ee2fa4553f6f493a122
BittVivek08 <vivekkimar706277@gmail.com>
2023-08-11 19:50:49 +0530

    jwt implementing

---
+++ src/main/java/com/rainier/config/JwtConstant.java
@@ -0,0 +1,9 @@
+package com.rainier.config;
+
+public class JwtConstant {
+	
+	public static final String SECRET_KEY = "ssdvhudyufbcdsncbhdcbc";
+	
+	public static final String JWT_HEADER = "Authorization";
+
+}
